213847086dd97b9cd9eb344314305c194949048f
Nick <nfears5798@gmail.com>
2022-09-23 20:27:59 -0400

    commit tree d82bbd52e8008339da978a2b8659b4f4bf310be9

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  // gives customers a free sample if the ride is less than or equal to 400 feet
+  // if
+  // less than or equal
+  // 400
+  let x = feet;
+  if (x <= 400) {
+    return "This one is on me!"
+  }
+  else if (x <= 2000) {
+    return "That will be twenty bucks."
+  }
+  else if (2000 < x && x <= 2500) {
+    return "I will gladly take your thirty bucks."
+  }
+  else {
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
 }
-
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip){
+    case 'generous':
+      return ('Thank you so much.')
+    case 'not as generous':
+      return ('Thank you.')
+    default:
+      return 'Bye.'
+  }
 }
